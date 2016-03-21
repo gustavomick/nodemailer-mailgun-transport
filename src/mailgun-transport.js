ce81aa9667885c0614378cb2160b36383a90e4bc
Sharry Stowell <sharry@molinto.com>
2016-03-21 13:41:34 +0000

    Formatting to 4 spaces

--- src/mailgun-transport.js
+++ src/mailgun-transport.js
@@ -1,60 +1,58 @@
+/*jslint node: true */
 'use strict';
 
 var Mailgun = require('mailgun-js');
 var packageData = require('../package.json');
 
 module.exports = function (options) {
-  return new MailgunTransport(options);
+    return new MailgunTransport(options);
 };
 
 function MailgunTransport(options) {
-  this.options = options || {};
-  this.name = 'Mailgun';
-  this.version = packageData.version;
+    this.options = options || {};
+    this.name = 'Mailgun';
+    this.version = packageData.version;
 
-  this.mailgun = Mailgun({
-    apiKey: this.options.auth.api_key,
-    domain: this.options.auth.domain || '',
-    proxy: this.options.proxy || ''
-  });
+    this.mailgun = Mailgun({
+        apiKey: this.options.auth.api_key,
+        domain: this.options.auth.domain || '',
+        proxy: this.options.proxy || ''
+    });
 }
 
-
 MailgunTransport.prototype.send = function send(mail, callback) {
-  var mailData = mail.data;
-  // convert nodemailer attachments to mailgun-js attachements
-  if(mailData.attachments){
-    var a, b, aa = [];
-    for(var i in mailData.attachments){
-      a = mailData.attachments[i];
-      b = new this.mailgun.Attachment({
-        data        : a.path || undefined,
-        filename    : a.filename || undefined,
-        contentType : a.contentType || undefined,
-        knownLength : a.knownLength || undefined
-      });
-
-      aa.push(b);
+    var mailData, a, b, aa, i, options;
+    mailData = mail.data;
+    // convert nodemailer attachments to mailgun-js attachements
+    if (mailData.attachments) {
+        aa = [];
+        for (i in mailData.attachments) {
+            a = mailData.attachments[i];
+            b = new this.mailgun.Attachment({
+                data        : a.path || undefined,
+                filename    : a.filename || undefined,
+                contentType : a.contentType || undefined,
+                knownLength : a.knownLength || undefined
+            });
+            aa.push(b);
+        }
+        mailData.attachment = aa;
     }
-    mailData.attachment = aa;
 
-  }
-  
-  var options = {
-    type       : mailData.type,
-    to         : mailData.to,
-    from       : mailData.from,
-    subject    : mailData.subject,
-    text       : mailData.text,
-    html       : mailData.html,
-    attachment : mailData.attachment
-  }
-  
-  if( mailData.bcc ){
-    options.bcc = mailData.bcc
-  }
+    options = {
+        type       : mailData.type,
+        to         : mailData.to,
+        from       : mailData.from,
+        subject    : mailData.subject,
+        text       : mailData.text,
+        html       : mailData.html,
+        attachment : mailData.attachment,
+        'h:Reply-To': mailData['headers']['Reply-To']
+    };
 
-  this.mailgun.messages().send(options, callback);
+    if (mailData.bcc) {
+        options.bcc = mailData.bcc;
+    }
 
+    this.mailgun.messages().send(options, callback);
 };
-
